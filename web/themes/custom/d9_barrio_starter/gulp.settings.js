f20e9c2c35bb69576edbd3d5641ea1a01a51c7e7
Richard Gilbert <gilbert@urbaninsight.com>
2022-04-29 10:53:31 -0700

    Updated Gulp and Sass.

--- web/themes/custom/d9_barrio_starter/gulp.settings.js
+++ web/themes/custom/d9_barrio_starter/gulp.settings.js
@@ -1,6 +1,6 @@
 // Copy this file to gulp.settings.local.js and replace the dev url with your own
 module.exports = {
     local: {
-        devUrl: 'your-dev-url.local'
+        devUrl: 'http://d9-starter-theme.lndo.site'
     }
 };
