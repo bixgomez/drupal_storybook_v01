0391fc4152d4ec90fb20eff449466a549c5bbc3d
Richard Gilbert <gilbert@urbaninsight.com>
2022-08-01 12:53:19 -0700

    Updates to modules and layouts.

---
+++ web/themes/custom/d9_barrio_starter/js/global.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.lacma_co = {
+    attach: function (context, settings) {
+    }
+  };
+
+})(jQuery, Drupal);
